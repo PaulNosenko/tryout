e767a61166838e2a28aa58c885d46f0f5c8714e1
Paul Nosenko <pavelgadzenko@gmail.com>
2019-01-07 21:43:00 +0100

    initial master

---
+++ src/main/java/api/TryoutApplication.java
@@ -0,0 +1,14 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryoutApplication.class, args);
+	}
+
+}
+
